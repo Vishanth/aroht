b0371383df9bd5000499d8ca644950abf904c1dc
visahan <visahan92@gmail.com>
2017-12-28 14:09:16 +1000

    Including facebook share option

--- index.php
+++ index.php
@@ -66,11 +66,21 @@ if($result = $db->query("SELECT * FROM since where date>'2007-03-11 18:00:00' or
   </head>
 
   <body id="page-top">
+  
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">SINCE LAST THORA won the BATTLE OF THE BLUES</a>
+		<div class="fb-share-button" data-href="http://since-last.com/index.php" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
       </div>
     </nav>
 
